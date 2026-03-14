755524e06a012cbe7d807a0f48715f9818bbd9bf
Santiago Rendon <santiagorendon361@gmail.com>
2026-03-14 16:08:26 -0400

    save df scala file

--- src/main/scala/part2dataframes/DataFramesBasicsV2.scala
+++ src/main/scala/part2dataframes/DataFramesBasicsV2.scala
@@ -1,5 +1,90 @@
 package part2dataframes
 
-object DataFramesBasicsV2 {
+import org.apache.spark.SparkContext.getOrCreate
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.{DoubleType, LongType, StringType, StructField, StructType}
+import part2dataframes.DataFramesBasics.spark
+
+object DataFramesBasicsV2 extends App {
+  // spark session is entry point to reading and writing data frames
+  val spark = SparkSession.builder()
+    .appName("DataFrames Basics")
+    // set spark master to local, executing locally so config is local
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  // reading a DF
+  val firstDF = spark.read
+    .format("json") // what we are reading
+    // in practice its better to provide schema, for example we can parse dates wrong since they can get treated as string
+    .option("inferSchema", "true")
+    .load("src/main/resources/data/cars.json")
+
+  // showing a DF
+  firstDF.show()
+  firstDF.printSchema()
+
+  // get rows
+  firstDF.take(10).foreach(println)
+
+  // spark types
+  val longType = LongType
+  val stringType = StringType
+  // ...
+
+  // create schema
+  val carsSchema = StructType(Array(
+    // nullable defaults to true so do not need to set it
+    StructField("Name", StringType, nullable = true),
+    StructField("Miles_per_Gallon", DoubleType),
+    StructField("Cylinders", LongType),
+    StructField("Displacement", DoubleType),
+    StructField("Horsepower", LongType),
+    StructField("Weight_in_lbs", LongType),
+    StructField("Acceleration", DoubleType),
+    StructField("Year", StringType),
+    StructField("Origin", StringType)
+  ))
+
+  // obtain a schema
+  val carsDFSchema = firstDF.schema
+
+
+  // read a DF with your schema
+  val carsDFWithSchema = spark.read
+    .format("json")
+    .schema(carsDFSchema)
+    .load("src/main/resources/data/cars.json")
+
+
+  // create rows by hand
+  val myRow = Row("chevrolet chevelle malibu",18,8,307,130,3504,12.0,"1970-01-01","USA")
+
+  // create DF from tuples
+  val cars = Seq(
+    ("chevrolet chevelle malibu",18,8,307,130,3504,12.0,"1970-01-01","USA"),
+    ("buick skylark 320",15,8,350,165,3693,11.5,"1970-01-01","USA"),
+    ("plymouth satellite",18,8,318,150,3436,11.0,"1970-01-01","USA"),
+    ("amc rebel sst",16,8,304,150,3433,12.0,"1970-01-01","USA"),
+    ("ford torino",17,8,302,140,3449,10.5,"1970-01-01","USA"),
+    ("ford galaxie 500",15,8,429,198,4341,10.0,"1970-01-01","USA"),
+    ("chevrolet impala",14,8,454,220,4354,9.0,"1970-01-01","USA"),
+    ("plymouth fury iii",14,8,440,215,4312,8.5,"1970-01-01","USA"),
+    ("pontiac catalina",14,8,455,225,4425,10.0,"1970-01-01","USA"),
+    ("amc ambassador dpl",15,8,390,190,3850,8.5,"1970-01-01","USA")
+  )
+  val manualCarsDF = spark.createDataFrame(cars) // schema auto-inferred
+
+  // note: DFs have schemas, rows do not
+
+  // create DFs with implicits
+  import spark.implicits._
+  val manualCarsDFWithImplicits = cars.toDF("Name", "MPG", "Cylinders", "Displacement", "HP", "Weight", "Acceleration", "Year", "CountryOrigin")
+
+
+
+
+
+
 
 }
